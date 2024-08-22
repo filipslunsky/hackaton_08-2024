b73d1f649314ab3dcc059762c8052ca7e1ddf9d2
Cynxos <cynxos@gmail.com>
2024-08-22 12:33:55 +0300

    Update Tables.sql

--- Tables.sql
+++ Tables.sql
@@ -1,21 +1,27 @@
 CREATE TABLE users (
-    ID SERIAL PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-	age VARCHAR(255),
-	weight VARCHAR(255)
+    user_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    birth_date DATE,
+    weight VARCHAR(255)
 );
 
 CREATE TABLE exercise (
-    ID SERIAL PRIMARY KEY,
+    exercise_id SERIAL PRIMARY KEY,
     exercise_type VARCHAR(255),
     exercise_reps VARCHAR(255),
-	exercise_sets VARCHAR(255)
+    exercise_sets VARCHAR(255),
+    exercise_date DATE,
+    fk_user_id INT,
+    CONSTRAINT fk_user FOREIGN KEY (fk_user_id) REFERENCES users(user_id) ON DELETE RESTRICT
 );
 
 CREATE TABLE food (
-    ID SERIAL PRIMARY KEY,
+    food_id SERIAL PRIMARY KEY,
     food_name VARCHAR(255),
     serving_size VARCHAR(255),
-	calories VARCHAR(255)
-)
+    calories VARCHAR(255),
+    food_date DATE,
+    fk_user_id INT,
+    CONSTRAINT fk_user_food FOREIGN KEY (fk_user_id) REFERENCES users(user_id) ON DELETE RESTRICT
+);
