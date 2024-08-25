48b2d65bc8143433685f6854eb785a385205c265
filipslunsky <fslunsky@gmail.com>
2024-08-25 11:02:48 +0200

    comments and docstring added for calulation_calories.py

--- calculations_calories.py
+++ calculations_calories.py
@@ -1,8 +1,9 @@
 import requests
 from auxiliary_functions import get_number_input
 
-
 def get_calories_burned(exercise_name, duration_in_minutes, height, weight, age, gender):
+    '''function uses free version of API and returns number of burned calories'''
+    # public API with a free key and ID
     url = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
     headers = {
@@ -18,7 +19,7 @@ def get_calories_burned(exercise_name, duration_in_minutes, height, weight, age,
         "height_cm": height,
         "age": age
     }
-
+    # since the API is free, it provides the calories info for 30 minutes and ignores the time parameter, the time_coef compensates for this
     time_coef = duration_in_minutes / 30
 
     response = requests.post(url, headers=headers, json=body)
@@ -34,6 +35,8 @@ def get_calories_burned(exercise_name, duration_in_minutes, height, weight, age,
         return 0
 
 def get_nutrients(food):
+    '''function uses free version of API and returns tuple (food, grams, calories)'''
+    # public API with a free key and ID
     api_url = "https://trackapi.nutritionix.com/v2/natural/nutrients"
     headers = {
         "x-app-id": "9be055c5",
@@ -54,14 +57,18 @@ def get_nutrients(food):
         name = ""
         serving_weight_grams = 0
         calories = 0
+    
     return name, serving_weight_grams, calories
 
 
 def get_calories_for_food(string):
+    ''''function parses user input of food'''
+    # cleaning the input from separating characters and making a list of keywords
     string = string.replace(",", "")
     string = string.replace("."," ")
     food_list = string.split(" ")
 
+    # trying the keywords and their pairs in the API and stroring unempty results
     results = []
     for i in range(4):
         sublist = food_list[0 + i : 2 + i]
@@ -69,18 +76,20 @@ def get_calories_for_food(string):
         result = get_nutrients(food)
         if result[0] != "":
             results.append(result)
-        
+    
+    # filtering out duplicate results and less accurate results (e.g. results for "chicken" are less accourate than results for "fried chicken")
     food_list = list(set(results))
-
     food_list.sort(key=lambda x: len(x[0]), reverse=True)
     filtered_list = []
     for item in food_list:
         if not any(item[0] in other[0] for other in filtered_list):
             filtered_list.append(item)
+    
     return filtered_list
 
 
 def calculate_total_calories(list_of_tuples_food):
+    '''function orders consumed food and calories in a dictionary'''
     food_intake = {}
     for item in list_of_tuples_food:
         quantity = get_number_input(f"How many grams of {item[0]} did you eat?  ")
@@ -89,6 +98,10 @@ def calculate_total_calories(list_of_tuples_food):
     
     return food_intake
 
-
+if __name__ == "__main__":
+    print(get_calories_burned("squats", 15, 175, 75, 30, "male"))
+    calories = get_calories_for_food("boiled chicken with rice")
+    print(calories)
+    print(calculate_total_calories(calories))
 
 
