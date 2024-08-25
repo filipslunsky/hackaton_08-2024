2f4d7b25d276421bcdbf505260426220e3d346c8
filipslunsky <fslunsky@gmail.com>
2024-08-25 09:44:19 +0200

    fixed typos in display_stats

--- interface_cli.py
+++ interface_cli.py
@@ -77,13 +77,13 @@ def display_stats(email, age, weight, height, bmi, daily_calories_quote, daily_e
     remaining_food = daily_calories_quote - food_results[1]
     remaining_exercise = daily_exercise_quote - exercise_info[2]
     print(f"""
-FOOD INTAKE:            {round(food_results[1], 2)} Kcal (in form of {food_results[0]})
+FOOD INTAKE:            {round(food_results[1], 2)} Kcal ( in form of {food_results[0]})
 OPTIMAL DAILY INTAKE:   {round(daily_calories_quote, 2)} Kcal
 REMAINING INTAKE:       {round(remaining_food, 2)} Kcal
-PHYSICAL ACTIVITY:      {round(exercise_info[2], 2)} cal (by doing {exercise_info[0]} for total of {exercise_info[1]} minutes)
+PHYSICAL ACTIVITY:      {round(exercise_info[2], 2)} cal ( by doing {exercise_info[0]} for total of {exercise_info[1]} minutes )
 DAILY GOAL:             {round(daily_exercise_quote, 2)} cal
 REMAINING EXERCISE:     {round(remaining_exercise, 2)} cal
-----------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------
 AGE:                    {age} years old
 BODY WEIGHT:            {weight} kg
 HEIGHT:                 {height} cm
